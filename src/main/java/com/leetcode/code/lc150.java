6ad1591565a4f9a1e84d7c4ba2e38bbfdc0a5ead
Alleyoops <hp917@qq.com>
2025-07-17 22:19:09 +0800

    测试提交正常

--- src/main/java/com/leetcode/code/lc150.java
+++ src/main/java/com/leetcode/code/lc150.java
@@ -9,6 +9,9 @@ import java.util.Deque;
 解释: 该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
  */
 public class lc150 {
+    public static void main(String[] args) {
+        System.out.println(evalRPN(new String[]{"2", "1", "+", "3", "*"}));
+    }
 
     /*
     add 方法是 Queue 接口的一部分，用于将一个元素插入到队列的末尾。
@@ -17,7 +20,7 @@ public class lc150 {
     push 方法是 Deque 接口的一部分，用于将一个元素插入到双端队列的头部。
     它模拟了栈的行为，将元素压入栈顶。
      */
-    public int evalRPN(String[] tokens) {
+    public static int evalRPN(String[] tokens) {
         Deque<Integer> deque = new ArrayDeque<>();
         for (String s : tokens) {
             switch (s) {
