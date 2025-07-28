a44f7634fd615fd9f30b2751fb7919cd83447ff5
Alleyoops <hp917@qq.com>
2025-07-28 23:54:53 +0800

    二叉树迭代法遍历lc101

--- src/main/java/com/leetcode/code/lc101.java
+++ src/main/java/com/leetcode/code/lc101.java
@@ -11,69 +11,60 @@ public class lc101 {
         //递归
         //DFS。左右中遍历
         //比较根节点的左子树和右子树是否镜像对称
-        if (root==null) return true;
+        if (root == null) return true;
         TreeNode leftTree = root.left;
         TreeNode rightTree = root.right;
-        return compareLAndR(leftTree,rightTree);
+        return compareLAndR(leftTree, rightTree);
 
     }
+
     //比较外侧：左子树的左节点VS右子树的右节点
     //比较内侧：左子树的右节点VS右子树的左节点
-    public boolean compareLAndR(TreeNode left, TreeNode right){
+    public boolean compareLAndR(TreeNode left, TreeNode right) {
 
-        boolean out,in;
-        if (left==null&&right==null) {
+        boolean out, in;
+        if (left == null && right == null) {
             return true;
-        }
-        else if (left==null||right==null) {
+        } else if (left == null || right == null) {
             return false;
-        } else if (left.val!= right.val){
+        } else if (left.val != right.val) {
             return false;
         } else {
-            out = compareLAndR(left.left,right.right);
-            in = compareLAndR(left.right,right.left);
-            return in&&out;
+            out = compareLAndR(left.left, right.right);
+            in = compareLAndR(left.right, right.left);
+            return in && out;
         }
     }
 
     //迭代法，层序遍历
-    public boolean compare(TreeNode root){
-        Deque<TreeNode> queue = new LinkedList<>();//双向队列
-        queue.add(root);
-        while (!queue.isEmpty()){
-            int layerNum = queue.size();
-            for (int i = 0; i < layerNum; i++) {
-                TreeNode node = queue.poll();
-                if (node.left!=null) {
-                    queue.add(node.left);
-                }
-                if (node.right!=null) {
-                    queue.add(node.right);
-                }
+    public boolean isSymmetric2(TreeNode root) {
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offerFirst(root.left);
+        deque.offerLast(root.right);
+        while (!deque.isEmpty()) {
+            TreeNode leftNode = deque.pollFirst();
+            TreeNode rightNode = deque.pollLast();
+            if (leftNode == null && rightNode == null) {
+                continue;
             }
-            int size = queue.size();
-            if (size%2!=0) return false;
-            for (int i = 0; i < size / 2; i++) {
-                TreeNode left = queue.pollLast();
-                TreeNode right = queue.pollFirst();
-                if (left.val!=right.val) return false;
-                TreeNode leftLeft = left.left;
-                TreeNode rightRight = right.right;
-                if (leftLeft!=null&&rightRight!=null){
-                    if (leftLeft.val!=rightRight.val) return false;
-                } else if (leftLeft==null&&rightRight==null) {
-                } else return false;
-
-                TreeNode leftRight = left.right;
-                TreeNode rightLeft = right.left;
-                if (leftRight!=null&&rightLeft!=null){
-                    if (leftRight.val!=rightLeft.val) return false;
-                } else if (leftRight==null&&rightLeft==null) {
-                } else return false;
-
+//            if (leftNode == null && rightNode != null) {
+//                return false;
+//            }
+//            if (leftNode != null && rightNode == null) {
+//                return false;
+//            }
+//            if (leftNode.val != rightNode.val) {
+//                return false;
+//            }
+            // 以上三个判断条件合并
+            if (leftNode == null || rightNode == null || leftNode.val != rightNode.val) {
+                return false;
             }
+            deque.offerFirst(leftNode.left);
+            deque.offerFirst(leftNode.right);
+            deque.offerLast(rightNode.right);
+            deque.offerLast(rightNode.left);
         }
         return true;
-
     }
 }
