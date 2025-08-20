fc44f7a88bf0d9602df95ae6ddd4712f8c3c8b5d
Alleyoops <hp917@qq.com>
2025-08-20 23:37:29 +0800

    回溯 lc17

--- src/main/java/com/leetcode/code/lc1_100/lc17.java
+++ src/main/java/com/leetcode/code/lc1_100/lc17.java
@@ -4,50 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class lc17 {
-    public static void main(String[] args) {
-        System.out.println(letterCombinations("23"));
-    }
-    public static List<String> letterCombinations(String digits) {
-        //用字符串数组构造字母映射
-        String[] map = new String[]{
-                "",//0
-                "",//1
-                "abc", // 2
-                "def", // 3
-                "ghi", // 4
-                "jkl", // 5
-                "mno", // 6
-                "pqrs", // 7
-                "tuv", // 8
-                "wxyz", // 9
-        };
+    String[] map = new String[]{
+            "",//0
+            "",//1
+            "abc", // 2
+            "def", // 3
+            "ghi", // 4
+            "jkl", // 5
+            "mno", // 6
+            "pqrs", // 7
+            "tuv", // 8
+            "wxyz", // 9
+    };
+    List<String> result = new ArrayList<>();
+    StringBuilder temp = new StringBuilder();
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return new ArrayList<>();
+        //构造数字数组
         int[] indexes = new int[digits.length()];
         for (int i = 0; i < digits.length(); i++) {
             indexes[i] = Integer.parseInt(String.valueOf(digits.charAt(i)));
         }
-        List<String> result = new ArrayList<>();
-        String temp = "";
-        backTrack(result, temp,indexes,map,0);
+        //递归回溯
+        backTrack(indexes,0);
         return result;
     }
-    public static void backTrack(List<String> result,String temp,int[] indexes,String[] map,int startIndex){
+    public void backTrack(int[] indexes,int startIndex){
         //终止条件
         if (temp.length()==indexes.length){
-            result.add(temp);
-            System.out.println("---");
+            result.add(temp.toString());
             return;
         }
         //处理逻辑
-        StringBuilder tempBuilder = new StringBuilder(temp);
-        for (int i = startIndex; i < indexes.length; i++) {
-            int index = indexes[startIndex];
-            String characters = map[index];
-            for (int j = 0; j < characters.length(); j++) {
-                tempBuilder.append(characters.charAt(j));
-                backTrack(result,temp,indexes,map,i+1);
-                tempBuilder.deleteCharAt(tempBuilder.length()-1);
-            }
+        //获取当前深度的数字对应的字母集合
+        int index = indexes[startIndex];
+        String s = map[index];
+        //循环处理当前集合各个字母，构造复合条件的字符串
+        for (int i = 0; i < s.length(); i++) {
+            temp.append(s.charAt(i));
+            //继续添加字符串的下一个字母。利用递归
+            backTrack(indexes,startIndex+1);
+            //回溯
+            temp.deleteCharAt(temp.length()-1);
         }
-        temp = tempBuilder.toString();
     }
 }
