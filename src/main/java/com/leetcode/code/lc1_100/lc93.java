f0c62b0d0f6f1f50e5d8aab0fe8b68e2f62c50d1
Alleyoops <hp917@qq.com>
2025-08-22 00:16:55 +0800

    回溯 lc93

--- src/main/java/com/leetcode/code/lc1_100/lc93.java
+++ src/main/java/com/leetcode/code/lc1_100/lc93.java
@@ -7,20 +7,39 @@ import java.util.List;
  */
 public class lc93 {
     List<String> result = new ArrayList<>();
-    StringBuilder sb = new StringBuilder();
+    StringBuilder sb_path = new StringBuilder();
     public List<String> restoreIpAddresses(String s) {
-        backTrack(s,0);
+        backTrack(s,0,new StringBuilder());
         return result;
     }
-    private void backTrack(String s,int startIndex){
+    private void backTrack(String s,int startIndex,StringBuilder sb){
         //终止条件
-        if (sb.length()==s.length()+4){
-            result.add(sb.toString());
+        if (startIndex==s.length()&&sb_path.length()==s.length()+4){
+            System.out.println("test");
+            sb_path.deleteCharAt(sb_path.length()-1);
+            result.add(sb_path.toString());
             return;
         }
         //处理逻辑
-        for (int i = startIndex; i < 3; i++) {
-
+        for (int i = startIndex; i < s.length(); i++) {
+            String sub = sb.append(s.charAt(i)).toString();
+            if (sub.length()==4) break;
+            int num = Integer.parseInt(sub);
+            //符合要求的情况一：单独0
+            if (num==0){
+                sb_path.append(sub);
+                sb_path.append(".");
+                backTrack(s,i+1,new StringBuilder());
+                sb_path.deleteCharAt(sb_path.length()-1);
+                break;
+            }
+            //情况二：1~255
+            if (1<=num&&num<=255){
+                sb_path.append(sub);
+                sb_path.append(".");
+                backTrack(s,i+1,new StringBuilder());
+                sb_path.deleteCharAt(sb_path.length()-1);
+            }
         }
     }
 }
