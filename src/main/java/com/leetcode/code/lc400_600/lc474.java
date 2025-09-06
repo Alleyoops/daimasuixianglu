613046b16aa2162cbcd063ec5b3001a292c00cd8
Alleyoops <hp917@qq.com>
2025-09-06 11:29:15 +0800

    贪心 lc474

---
+++ src/main/java/com/leetcode/code/lc400_600/lc474.java
@@ -0,0 +1,7 @@
+package com.leetcode.code.lc400_600;
+
+public class lc474 {
+    public int findMaxForm(String[] strs, int m, int n) {
+
+    }
+}
--- src/main/java/com/leetcode/code/lc400_600/lc494.java
+++ src/main/java/com/leetcode/code/lc400_600/lc494.java
@@ -1,13 +1,24 @@
 package com.leetcode.code.lc400_600;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class lc494 {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
         for (int num : nums) sum += num;
-        if (sum<target||-sum>target) return 0;
-
+        if (Math.abs(target)>sum) return 0;
+        //不是简单的01背包问题，而是求有几种方法
+        if ((sum+target)%2!=0) return 0;
+        int pkg = (sum+target)/2;
+        //dp数组，dp[i][j]表示背包容量为j时可装下物品0~i的最多方法数
+        int[] dp = new int[pkg+1];
+        //初始化
+        dp[0]=1;//不放物品时dp为：1、0、0、0...
+        for (int num : nums) {//一个物品、两个物品...
+            for (int j = pkg; j >= 0; j--) {//最大方法数
+                if (j >= num) {
+                    dp[j] += dp[j - num];
+                }
+            }
+        }
+        return dp[pkg];
     }
 }
