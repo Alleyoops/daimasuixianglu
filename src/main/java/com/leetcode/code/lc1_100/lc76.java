2fdd50419625e4483f4547d3913bf331605acd6c
Alleyoops <hp917@qq.com>
2025-12-23 15:22:47 +0800

    -

--- src/main/java/com/leetcode/code/lc1_100/lc76.java
+++ src/main/java/com/leetcode/code/lc1_100/lc76.java
@@ -3,33 +3,42 @@ package com.leetcode.code.lc1_100;
 import java.util.HashMap;
 
 public class lc76 {
-    public String minWindow(String s, String t) {
-        HashMap<Character,Integer> tMap = new HashMap<>();
-        //统计t的字母频率
-        for(int i = 0;i<t.length();i++){
-            tMap.put(t.charAt(i),tMap.getOrDefault(t.charAt(i),0)+1);
+    public static void main(String[] args) {
+        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+    }
+
+    public static String minWindow(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        if (m < n) return "";
+        int[] need = new int[128];
+        for (char c : t.toCharArray()) {
+            need[c]++;
         }
-        int left = -1;
-        int minLen = s.length();
-        int ansLeft = -1;
-        int ansRight = s.length();
-        for(int right = 0;right<=s.length()-1&&left<right;right++){
-            if(check(s,t,tMap,left,right)){
-                if(right-left<=minLen){
-                    minLen = right-left;
-                    ansLeft = left;
-                    ansRight = right;
+        int left = 0;
+        int right = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+        int count = 0;
+        for (; right < m; right++) {
+            char c = s.charAt(right);
+            need[c]--;
+            if (need[c]>=0){
+                count++;
+            }
+            while (count==n){
+                if(right-left+1<minLen){
+                    minLen = right-left+1;
+                    start = left;
+                }
+                char d = s.charAt(left);
+                need[d]++;
+                if (need[d]>0){
+                    count--;
                 }
                 left++;
             }
         }
-        return s.substring(ansLeft+1,ansRight);
-    }
-    public boolean check(String s,String t, HashMap<Character,Integer> tMap,int left,int right){
-        if(right-left<t.length()) return false;
-        for(int i = left+1;i<=right;i++){
-            tMap.remove(s.charAt(i));
-        }
-        return tMap.isEmpty();
+        return minLen==Integer.MAX_VALUE?"":s.substring(start,start+minLen);
     }
 }
