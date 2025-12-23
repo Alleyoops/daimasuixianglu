d7eef9283a5b7fb998d38d44c597d0e84992b820
Alleyoops <hp917@qq.com>
2025-12-23 11:17:31 +0800

    -

---
+++ src/main/java/com/leetcode/code/lc1_100/lc76.java
@@ -0,0 +1,39 @@
+package com.leetcode.code.lc1_100;
+
+import java.util.HashMap;
+
+public class lc76 {
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> tMap = new HashMap<>();
+        //统计t的字母频率
+        for(int i = 0;i<t.length();i++){
+            tMap.put(t.charAt(i),tMap.getOrDefault(t.charAt(i),0)+1);
+        }
+        int left = -1;
+        int minLen = s.length();
+        int ansLeft = -1;
+        int ansRight = s.length();
+        for(int right = 0;right<=s.length()-1&&left<right;right++){
+            if(check(s,t,tMap,left,right)){
+                if(right-left<=minLen){
+                    minLen = right-left;
+                    ansLeft = left;
+                    ansRight = right;
+                }
+                left++;
+            }
+        }
+        String ansStr = s.substring(ansLeft+1,ansRight-ansLeft);
+        return ansStr;
+    }
+    public boolean check(String s,String t, HashMap<Character,Integer> tMap,int left,int right){
+        if(right-left<t.length()) return false;
+        for(int i = left+1;i<=right;i++){
+            if(tMap.containsKey(s.charAt(i))){
+                tMap.remove(s.charAt(i));
+            }
+        }
+        if(tMap.size()==0) return true;
+        else return false;
+    }
+}
